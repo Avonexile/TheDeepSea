ceef8641d74ed06aa1ef35d7d7b7670d3feda2c4
Avonexile <arnevanommen@outlook.com>
2020-02-06 09:20:02 +0100

    Implement gaia

--- Assets/Plugins/SUIMONO - WATER SYSTEM 2/SCRIPTS/SuimonoGaia.cs
+++ Assets/Plugins/SUIMONO - WATER SYSTEM 2/SCRIPTS/SuimonoGaia.cs
@@ -6,6 +6,8 @@ using System;
 using System.Reflection;
 using UnityEditor;
 
+using Gaia.GX.RaphaelErnaelsten;
+
 namespace Gaia.GX.TanukiDigital
 {
     /// <summary>
@@ -54,119 +56,119 @@ namespace Gaia.GX.TanukiDigital
 
 
             //Get scene info
-            GaiaSceneInfo sceneInfo = GaiaSceneInfo.GetSceneInfo();
+        //    GaiaSceneInfo sceneInfo = GaiaSceneInfo.GetSceneInfo();
 
 
-            //INSTALL Suimono Module
-            GameObject suimonoPrefab = Gaia.Utils.GetAssetPrefab("SUIMONO_Module");
-            if (suimonoPrefab == null)
-            {
-                Debug.LogWarning("Unable to locate SuimonoModule - Aborting!");
-                return;
-            }
+        //    //INSTALL Suimono Module
+        //    GameObject suimonoPrefab = Gaia.Utils.GetAssetPrefab("SUIMONO_Module");
+        //    if (suimonoPrefab == null)
+        //    {
+        //        Debug.LogWarning("Unable to locate SuimonoModule - Aborting!");
+        //        return;
+        //    }
 
-            //See if we can locate it
-            if (GameObject.Find("SUIMONO_Module") != null)
-            {
-                Debug.LogWarning("Suimono Module already in scene - Aborting!");
-                return;
-            }
+        //    //See if we can locate it
+        //    if (GameObject.Find("SUIMONO_Module") != null)
+        //    {
+        //        Debug.LogWarning("Suimono Module already in scene - Aborting!");
+        //        return;
+        //    }
 
-            //See if we can create it
-            GameObject suimonoObj = Instantiate(suimonoPrefab);
-            if (suimonoObj == null)
-            {
-                Debug.LogWarning("Unable to create Suimono Module object - Aborting!");
-                return;
-            }
-            else
-            {
-                suimonoObj.name = "SUIMONO_Module";
-            }
-
-
-
-
-            //INSTALL Suimono Surface
-            GameObject surfacePrefab = Gaia.Utils.GetAssetPrefab("SUIMONO_Surface");
-            if (suimonoPrefab == null)
-            {
-                Debug.LogWarning("Unable to locate SuimonoSurface - Aborting!");
-                return;
-            }
-
-            //See if we can locate it
-            if (GameObject.Find("SUIMONO_Surface") != null)
-            {
-                Debug.LogWarning("Suimono Surface already in scene - Aborting!");
-                return;
-            }
-
-            //See if we can create it
-            GameObject surfaceObj = Instantiate(surfacePrefab);
-            if (surfaceObj == null)
-            {
-                Debug.LogWarning("Unable to create Suimono Surface object - Aborting!");
-                return;
-            }
-            else
-            {
-                //set surface
-                surfaceObj.name = "SUIMONO_Surface";
-
-                //set surface position
-                surfaceObj.transform.position = new Vector3(0.0f,0.0f,0.0f);
-
-                //Set Water Y-Height and expansion
-                if (sceneInfo != null)
-                {
-                    surfaceObj.transform.position = new Vector3(sceneInfo.m_centrePointOnTerrain.x, sceneInfo.m_seaLevel, sceneInfo.m_centrePointOnTerrain.z);
-                    surfaceObj.transform.localScale = sceneInfo.m_sceneBounds.extents*1;
-                }
-
-
-                //set surface-specific settings
-                var surfaceObject = surfaceObj.GetComponent<Suimono.Core.SuimonoObject>();
-                if (surfaceObject != null)
-                {
-                    //set surface as infinite ocean
-                    surfaceObject.typeIndex = 0;
-                }
-
-            }
+        //    //See if we can create it
+        //    GameObject suimonoObj = Instantiate(suimonoPrefab);
+        //    if (suimonoObj == null)
+        //    {
+        //        Debug.LogWarning("Unable to create Suimono Module object - Aborting!");
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        suimonoObj.name = "SUIMONO_Module";
+        //    }
 
 
 
 
-            //See if we can configure Module
-            //var suimonoModule = suimonoObj.GetComponent<Suimono.Core.SuimonoModule>();
-            var suimonoModule = (Suimono.Core.SuimonoModule) FindObjectOfType(typeof(Suimono.Core.SuimonoModule));
-            if (suimonoModule != null)
-            {
+        //    //INSTALL Suimono Surface
+        //    GameObject surfacePrefab = Gaia.Utils.GetAssetPrefab("SUIMONO_Surface");
+        //    if (suimonoPrefab == null)
+        //    {
+        //        Debug.LogWarning("Unable to locate SuimonoSurface - Aborting!");
+        //        return;
+        //    }
 
-                //Set the camera
-                Camera camera = Camera.main;
-                if (camera == null)
-                {
-                    camera = FindObjectOfType<Camera>();
-                }
-                if (camera != null)
-                {
-                    suimonoModule.manualCamera = camera.transform;
-                }
+        //    //See if we can locate it
+        //    if (GameObject.Find("SUIMONO_Surface") != null)
+        //    {
+        //        Debug.LogWarning("Suimono Surface already in scene - Aborting!");
+        //        return;
+        //    }
+
+        //    //See if we can create it
+        //    GameObject surfaceObj = Instantiate(surfacePrefab);
+        //    if (surfaceObj == null)
+        //    {
+        //        Debug.LogWarning("Unable to create Suimono Surface object - Aborting!");
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        //set surface
+        //        surfaceObj.name = "SUIMONO_Surface";
+
+        //        //set surface position
+        //        surfaceObj.transform.position = new Vector3(0.0f,0.0f,0.0f);
+
+        //        //Set Water Y-Height and expansion
+        //        if (sceneInfo != null)
+        //        {
+        //            surfaceObj.transform.position = new Vector3(sceneInfo.m_centrePointOnTerrain.x, sceneInfo.m_seaLevel, sceneInfo.m_centrePointOnTerrain.z);
+        //            surfaceObj.transform.localScale = sceneInfo.m_sceneBounds.extents*1;
+        //        }
 
 
-                //Add scene directional light if it exists
-                Light lightObj = GameObject.Find("Directional Light").GetComponent<Light>();
+        //        //set surface-specific settings
+        //        var surfaceObject = surfaceObj.GetComponent<Suimono.Core.SuimonoObject>();
+        //        if (surfaceObject != null)
+        //        {
+        //            //set surface as infinite ocean
+        //            surfaceObject.typeIndex = 0;
+        //        }
 
-                if (lightObj != null){
-                    suimonoModule.setLight = lightObj;
-                } else {
-                    Debug.Log("Suimono: Could not find scene directional light through automatic detection, please set manually instead.");
-                }
+        //    }
 
 
-            }
+
+
+        //    //See if we can configure Module
+        //    //var suimonoModule = suimonoObj.GetComponent<Suimono.Core.SuimonoModule>();
+        //    var suimonoModule = (Suimono.Core.SuimonoModule) FindObjectOfType(typeof(Suimono.Core.SuimonoModule));
+        //    if (suimonoModule != null)
+        //    {
+
+        //        //Set the camera
+        //        Camera camera = Camera.main;
+        //        if (camera == null)
+        //        {
+        //            camera = FindObjectOfType<Camera>();
+        //        }
+        //        if (camera != null)
+        //        {
+        //            suimonoModule.manualCamera = camera.transform;
+        //        }
+
+
+        //        //Add scene directional light if it exists
+        //        Light lightObj = GameObject.Find("Directional Light").GetComponent<Light>();
+
+        //        if (lightObj != null){
+        //            suimonoModule.setLight = lightObj;
+        //        } else {
+        //            Debug.Log("Suimono: Could not find scene directional light through automatic detection, please set manually instead.");
+        //        }
+
+
+        //    }
 
         }
 
