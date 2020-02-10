609d2d1c459b7017049a3b54e88fd77fcb96bf95
Avonexile <arnevanommen@outlook.com>
2020-02-10 15:18:11 +0100

    Started working on chunks

---
+++ Assets/Scripts/WorldGeneration/Chunk.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Chunk
+{
+    public int ChunkID;
+
+    public Vector3 TopLeftCoords;
+    public Vector3 BottomRightCoords;
+    public Vector3 Center;
+
+    private int _maxPropsInChunk = 10;
+    private int _minPropsInChunk = 1;
+
+    public List<GameObject> Props = new List<GameObject>();
+
+    #region Properties
+    public int MaxPropsInChunk
+    {
+        get
+        {
+            return _maxPropsInChunk;
+        }
+    }
+    public int MinPropsInChunk
+    {
+        get
+        {
+            return _minPropsInChunk;
+        }
+    }
+    #endregion 
+
+    public Chunk (int index, Vector3 TopleftPos, Vector3 BottomRightPos)
+    {
+        ChunkID = index;
+        TopLeftCoords = TopleftPos;
+        BottomRightCoords = BottomRightPos;
+
+
+        Center = new Vector3(BottomRightCoords.x + TopLeftCoords.x, 0, TopLeftCoords.z + BottomRightCoords.z);
+    }
+    public Chunk(GameObject Prop)
+    {
+        Props.Add(Prop);
+    }
+}
---
+++ Assets/Scripts/WorldGeneration/EnvironmentGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentGenerator : MonoBehaviour
+{
+    public float ChunkSize;
+
+    public int MaxChunkLoaded;
+    private int LoadedChunks = 0;
+
+    public List<Chunk> Chunks = new List<Chunk>();
+    public List<Chunk> CurrentLoadedChunks = new List<Chunk>();
+
+    public Chunk CurrentChunk;
+
+    public float ChunkLoadingTime;
+
+    public Transform playerObject;
+
+    private void Awake()
+    {
+        //Start coroutine
+        StartCoroutine(LoadChunks());
+    }
+    private IEnumerator LoadChunks ()
+    {
+        while(LoadedChunks < MaxChunkLoaded)
+        {
+            if(Chunks.Count == 0)
+            {
+                //Create chunks
+                CurrentChunk = new Chunk(0, new Vector3(-ChunkSize / 2, 0, ChunkSize / 2), new Vector3(ChunkSize / 2, 0, -ChunkSize / 2));
+                CurrentLoadedChunks.Add(CurrentChunk);
+                Chunks.Add(CurrentChunk);
+
+
+
+                //Spawn Props in chunk
+            }
+
+            //Load Chunk
+
+            yield return new WaitForSeconds(ChunkLoadingTime);
+        }
+        StopCoroutine(LoadChunks());
+    }
+    public void OnDrawGizmos ()
+    {
+        foreach (var item in Chunks)
+        {
+            Gizmos.DrawCube(item.Center, new Vector3(ChunkSize , 0, ChunkSize));
+
+            Gizmos.DrawSphere(item.Center, 5f);
+            Gizmos.DrawLine(item.TopLeftCoords, item.BottomRightCoords);
+
+        }
+    }
+}
