b1fb8013f80108c91a453eeb622b5d5f495d4f3f
Avonexile <arnevanommen@outlook.com>
2020-03-02 15:33:20 +0100

    Deleted errors and prepared for tweaking

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Threading;
-using UnityEngine.InputSystem;
+
 
 public class PlayerMovementController : MonoBehaviour
 {
