93beb7817dd98a6d57d9d5da35161a4313f17b93
Avonexile <arnevanommen@outlook.com>
2020-03-11 12:39:50 +0100

    Implementing fish flocks

---
+++ Assets/Editor/FishSpawnerEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FishSpawner))]
+public class FishSpawnerEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        Draw();
+    }
+    void Draw ()
+    {
+        FishSpawner spawner = (FishSpawner)target;
+
+        for (int i = 0; i < spawner.spawnPositions.Count; i++)
+        {
+            Handles.DrawSphere(i, spawner.spawnPositions[i].position, Quaternion.identity, 5f);
+        }
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        FishSpawner spawner = (FishSpawner)target;
+
+        if (GUILayout.Button("Add fish"))
+        {
+            Undo.RecordObject(spawner, "Add fish");
+            spawner.AddFish();
+        }
+        if (GUILayout.Button("Add position"))
+        {
+            Undo.RecordObject(spawner, "Add position");
+            spawner.AddPosition();
+            //EditorUtility.SetDirty(tar);
+        }
+    }
+}
---
+++ Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    public List<GameObject> fishCatalogue;
+    public List<Transform> spawnPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void AddPosition ()
+    {
+       
+
+    }
+    public void AddFish ()
+    {
+        fishCatalogue.Add(null);
+    }
+}
