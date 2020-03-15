1a396039264e0c3c6665a9f34a22913de63ff698
Avonexile <arnevanommen@outlook.com>
2020-03-15 19:17:53 +0100

    Treasure finder start

---
+++ Assets/Scripts/TreasureFinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureFinder : MonoBehaviour
+{
+    private GameObject treasure;
+
+    private float distance;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Treasure")
+        {
+            //Vibrate
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Treasure")
+        {
+            if(treasure == null)
+                treasure = other.gameObject;
+
+            //Check distance between player and treasure
+            distance = Vector3.Distance(transform.position, other.transform.position);
+            
+            //Vibrate depending on distance
+        }
+    }
+}
