8a02ac6bfdedb5d34599c020815ea84e8712e3a7
Avonexile <arnevanommen@outlook.com>
2020-03-16 13:01:30 +0100

    Improved flock behaviour

--- Assets/Scripts/FlockBehaviour.cs
+++ Assets/Scripts/FlockBehaviour.cs
@@ -6,20 +6,66 @@ public class FlockBehaviour : MonoBehaviour
 {
     public SplineWalker spline;
 
+    public AnimationCurve escapeSpeedCurve;
+
+    private float duration;
+
+    private bool _speeding;
+
+    #region Properties
+    public bool Speeding
+    {
+        get
+        {
+            return _speeding;
+        }
+        set
+        {
+            _speeding = value;
+        }
+    }
+    #endregion 
+
     private void Start()
     {
         spline = GetComponent<SplineWalker>();
+        duration = spline.duration;
         GetComponent<Collider>().isTrigger = true;
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            spline.duration = spline.duration * 0.5f;
+            _speeding = true;
 
             //Reset after some time
-
-            //Slightly accelerate and deaccelerate over time
+            StartCoroutine(Escape());
         }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //_speeding = false;
+        }
+    }
+
+    private IEnumerator Escape ()
+    {
+        float time = 0;
+
+        while(_speeding)
+        {
+            Debug.Log("Run");
+            time += Time.deltaTime;
+
+            spline.duration = escapeSpeedCurve.Evaluate(time);
+
+            if (spline.duration > 20)
+                _speeding = false;
+
+            yield return null;
+        }
+        StopCoroutine(Escape());
     }
 }
