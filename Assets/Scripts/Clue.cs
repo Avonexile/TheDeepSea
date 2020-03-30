7ca2cd7363564c1bc51850fa10d50c6ed108e2e8
Avonexile <arnevanommen@outlook.com>
2020-03-30 14:52:36 +0200

    Added clue text to scenes

--- Assets/Scripts/Clue.cs
+++ Assets/Scripts/Clue.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Clue : MonoBehaviour
 {
     public bool HasBeenRead;
 
+    [TextArea(5, 15)]
     public string Text;
 }
