c7eb68f80f646f4e8d1bf638a7d24cb868613fc9
Avonexile <arnevanommen@outlook.com>
2020-05-13 14:14:06 +0200

    Added ui, new treasure finder vibration

--- Assets/Scripts/Clue.cs
+++ Assets/Scripts/Clue.cs
@@ -8,7 +8,14 @@ public class Clue : MonoBehaviour
     //See if it had been read already
     public bool HasBeenRead;
 
+    public float Speed;
+
     //The text that can be read
     [TextArea(5, 15)]
     public string Text;
+
+    public void FixedUpdate ()
+    {
+        transform.Rotate(new Vector3(0, 5 * Time.deltaTime * Speed, 0));
+    }
 }
